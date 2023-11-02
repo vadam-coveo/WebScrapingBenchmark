d03efebe0b761eed8772cbd07ed203ba00d6dd57
Dominic Berube <dberube@coveo.com>
2023-11-02 10:24:26 -0400

    Refact result reporter

---
+++ Framework/Reporting/LazyValueConverter.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace WebScrapingBenchmark.Framework.Reporting
+{
+    public class LazyValueConverter<T> : DefaultTypeConverter
+    {
+        public override string? ConvertToString(object? value, IWriterRow row, MemberMapData memberMapData)
+        {
+            return ((Lazy<T>)value)?.Value?.ToString();
+        }
+    }
+}
--- Framework/UrlScrapingResults/ScrapingTimingResults.cs
+++ Framework/UrlScrapingResults/ScrapingTimingResults.cs
@@ -1,4 +1,6 @@
-﻿using WebScrapingBenchmark.Framework.ScenarioRunner;
+﻿using CsvHelper.Configuration.Attributes;
+using WebScrapingBenchmark.Framework.Reporting;
+using WebScrapingBenchmark.Framework.ScenarioRunner;
 
 namespace WebScrapingBenchmark.Framework.UrlScrapingResults;
 
@@ -8,25 +10,35 @@ public class ScrapingTimingResults : BaseUrlScrapingResult
     public TimeSpan LoadTiming;
     public TimeSpan GetHtmlResultTiming;
 
-
+    [Ignore]
     public List<ElementTiming> MetadataExtractionTiming = new();
 
+    [Ignore]
     public List<ElementTiming> ContentExclusionTiming = new();
 
+    [TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
     public Lazy<TimeSpan> AverageMetadataExtraction => new Lazy<TimeSpan>(() => MetadataExtractionTiming.Select(timing => timing.Duration).Average());
+    [TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
     public Lazy<TimeSpan> AverageContentExclusion => new Lazy<TimeSpan>(() => ContentExclusionTiming.Select(timing => timing.Duration).Average());
 
+    /*[TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
     public Lazy<TimeSpan> FastestMetadataExtraction => new Lazy<TimeSpan>(() => MetadataExtractionTiming.Select(timing => timing.Duration).Min());
+    [TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
     public Lazy<TimeSpan> FastestContentExclusion => new Lazy<TimeSpan>(() => ContentExclusionTiming.Select(timing => timing.Duration).Min());
 
+    [TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
     public Lazy<TimeSpan> SlowestMetadataExtraction => new Lazy<TimeSpan>(() => MetadataExtractionTiming.Select(timing => timing.Duration).Max());
-    public Lazy<TimeSpan> SlowestContentExclusion => new Lazy<TimeSpan>(() => ContentExclusionTiming.Select(timing => timing.Duration).Max());
+    [TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
+    public Lazy<TimeSpan> SlowestContentExclusion => new Lazy<TimeSpan>(() => ContentExclusionTiming.Select(timing => timing.Duration).Max());*/
 
+    /*[TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
     public Lazy<TimeSpan> MetadataExtractionSum => new Lazy<TimeSpan>(() => TimeSpan.FromMilliseconds(MetadataExtractionTiming.Sum(timing => timing.Duration.TotalMilliseconds)));
 
+    [TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
     public Lazy<TimeSpan> ContentExclusionSum => new Lazy<TimeSpan>(() => TimeSpan.FromMilliseconds(ContentExclusionTiming.Sum(timing => timing.Duration.TotalMilliseconds)));
 
-    public Lazy<TimeSpan> TotalScrapingTime => new Lazy<TimeSpan>(() => LoadTiming + MetadataExtractionSum.Value + ContentExclusionSum.Value + GetHtmlResultTiming);
+    [TypeConverter(typeof(LazyValueConverter<TimeSpan>))]
+    public Lazy<TimeSpan> TotalScrapingTime => new Lazy<TimeSpan>(() => LoadTiming + MetadataExtractionSum.Value + ContentExclusionSum.Value + GetHtmlResultTiming);*/
 
     public ScrapingTimingResults(string url, string scenarioName, string scraperName) : base(url, scenarioName, scraperName)
     {
